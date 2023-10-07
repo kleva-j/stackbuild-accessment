cf8f6593ec72a03c7a8db4742ad4e0dc6bcc33f2
kleva-j <kasmickleva@gmail.com>
2023-10-07 19:21:44 +0100

    feat: setup deployment to vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,56 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src *.s3.amazonaws.com;
+  connect-src *;
+  font-src 'self';
+  frame-src giscus.app
+`;
 
-module.exports = nextConfig;
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+module.exports = {
+  reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  eslint: {
+    dirs: ["app", "components", "layouts", "scripts"],
+  },
+  images: { domains: ["picsum.photos"] },
+  async headers() {
+    return [{ source: "/(.*)", headers: securityHeaders }];
+  },
+};
