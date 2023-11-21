ad6ac06283e46ba084aec0520a1f9745ad978b3b
kleva-j <kasmickleva@gmail.com>
2023-11-21 15:01:28 +0100

    feat: imlement server-actions to replace tanstack query

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,123 @@
-import { getServerSession } from "next-auth";
+"use server";
 
-export default async function ServerActionPage() {
-  const saveNewPost = async () => {
-    "use server";
-    const session = await getServerSession();
-    return session?.user?.name || "Not signed in";
-  };
+import type { $InsertPost, $UpdatePost } from "@/lib/types";
+
+import { checkIfAuthed, handleAuthState } from "@/lib/auth";
+import { revalidatePath } from "next/cache";
+
+import prisma from "@/lib/prisma";
+
+export async function GetPostByUserAction(userId: string) {
+  try {
+    const isAuthed = await checkIfAuthed();
+
+    const posts = await prisma.post.findMany({
+      where: { userId, ...(!isAuthed ? { published: true } : {}) },
+      include: {
+        user: !!isAuthed,
+        likes: true,
+        comments: { orderBy: { createdAt: "desc" }, include: { user: true } },
+      },
+    });
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function GetSinglePostAction(postId: string) {
+  try {
+    const posts = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { user: true, comments: true },
+    });
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function GetAllPostsAction() {
+  try {
+    const posts = await prisma.post.findMany({
+      include: { likes: true, comments: true },
+      orderBy: { createdAt: "desc" },
+    });
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+type PaginatedParams = {
+  skip: number;
+  take: number;
+  cursor?: { id: string };
+};
+export async function GetPaginatedPostAction({
+  skip,
+  take,
+  cursor,
+}: PaginatedParams) {
+  try {
+    const [posts, total] = await prisma.$transaction([
+      prisma.post.findMany({
+        skip,
+        take,
+        cursor,
+        where: { published: true },
+        include: { likes: true, comments: true },
+        orderBy: { createdAt: "desc" },
+      }),
+      prisma.post.count(),
+    ]);
+
+    return { pagination: { total }, data: posts };
+  } catch (error) {
+    throw error;
+  }
+}
+export async function DeletePostAction() {
+  try {
+    const { id } = await handleAuthState();
+
+    await prisma.post.delete({ where: { id } });
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function EditPostAction(id: string, data: $UpdatePost) {
+  try {
+    const { id } = await handleAuthState();
+
+    const { title, content, published = false } = data;
+
+    await prisma.post.update({
+      where: { id },
+      data: { title, content, published },
+    });
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function CreatePostAction(data: $InsertPost) {
+  try {
+    const { id } = await handleAuthState();
+
+    const { title, content, published } = data;
+
+    await prisma.post.create({
+      data: { title, content, published, user: { connect: { id } } },
+    });
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    throw error;
+  }
 }
--- src/app/editor/[...action]/page.tsx
+++ src/app/editor/[...action]/page.tsx
@@ -1,29 +1,20 @@
-import { getSinglePostByUser } from "@/lib/posts";
+import { GetSinglePostAction } from "@/app/actions";
 import { Editor } from "@/components/Editor";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/nextAuth";
+import { checkIfAuthed } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { Post } from "@prisma/client";
 
-import prisma from "@/lib/prisma";
-
 const getPostByUser = async (id: string): Promise<Post> => {
   try {
-    const session = await getServerSession(authOptions);
+    const user = await checkIfAuthed();
 
-    if (!session) redirect("/api/auth/signin");
+    if (!user) redirect("/api/auth/signin");
 
-    const user = await prisma.user.findFirst({
-      where: { email: session?.user?.email },
-    });
-
-    if (!user) throw new Error("User not found!");
-
-    const post = await getSinglePostByUser(id, user.id);
+    const post = await GetSinglePostAction(id);
 
     if (!post) throw new Error("Post not found!");
 
-    return post[0];
+    return post;
   } catch (err: unknown) {
     throw err;
   }
